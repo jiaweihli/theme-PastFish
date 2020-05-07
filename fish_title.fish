16e4f5e3bc19dce8f4c7dcaebb785e82923ae858
Kevin Li <jiawei.h.li@gmail.com>
2020-05-07 13:37:23 -0700

    Update fish_title.fish

--- fish_title.fish
+++ fish_title.fish
@@ -2,5 +2,5 @@ function fish_title
   # Customize the title bar of the terminal window.
   # NOTICE:
   # oh-my-fish's theme dont work with tmux or screen title ... see `fish_prompt.fish`'s tmux, screen title part (around line 83)
-  echo '['(prompt_pwd)']'
+  echo '<'(hostname | cut -d . -f 1)'> ['(prompt_pwd)']'
 end
